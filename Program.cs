176cd8adaa8864b21c80a520ee11a209bc52a849
SalsaNP777 <salsa.nputr777@gmail.com>
2024-05-21 12:35:57 +0700

    finish

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 15;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 4 == 0)
+            {
+                Console.WriteLine("OKYES");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("OK");
+            }
+            else if (i % 4 == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
